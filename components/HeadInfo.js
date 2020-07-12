fad2fa6e11b29622ef63b844269bb771fc1d267a
Daniel Meireles <httpdaniel@yahoo.com.br>
2020-07-12 06:13:18 -0300

    Meta description

--- components/HeadInfo.js
+++ components/HeadInfo.js
@@ -1,17 +1,14 @@
 export default function HeadeInfo(){
     return(
         <>
-        
             <meta name="theme-color" content="#337fed" />
-            
+            <meta name="description" content="Site desenvolvido para a fase 7 do curso HcodeLab" />
+            <meta name="msapplication-TileImage" content="/ms-icon-144x144.png" />
             <link rel="icon" type="image/png" sizes="192x192"  href="/favicon/android-icon-192x192.png" />
             <link rel="icon" type="image/png" sizes="32x32" href="/favicon/favicon-32x32.png" />
             <link rel="icon" type="image/png" sizes="96x96" href="/favicon/favicon-96x96.png" />
             <link rel="icon" type="image/png" sizes="16x16" href="/favicon/favicon-16x16.png" />
-            <link rel="manifest" href="/favicon/manifest.json" />
-            <meta name="msapplication-TileImage" content="/ms-icon-144x144.png" />
-            <meta name="description" content="Site desenvolvido para a fase 7 do curso HcodeLab" />
-            
+            <link rel="manifest" href="/favicon/manifest.json" />  
         </>
     )
 }
