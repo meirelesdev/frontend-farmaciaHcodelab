b209d1117fd5f0a8b38b7afaadd3165a5f2a6d68
Daniel Meireles <httpdaniel@yahoo.com.br>
2020-07-12 06:40:50 -0300

    Mais otimizações

--- components/admin/Footer.js
+++ components/admin/Footer.js
@@ -4,7 +4,7 @@ export default function Footer({ menuState }) {
 
     return (
         <footer id={styles.footer} className={menuState ? styles.open : ''}>
-            <p className={styles.rights}>2020 © Hcode. All rights reserved.</p>
+            <p className={styles.rights}>2020 © Farmacia Hcode. All rights reserved.</p>
         </footer>
     )
 
