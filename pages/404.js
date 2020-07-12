82bbc4524aa36db423c4924bb32fd349b5c0a381
Daniel Meireles <httpdaniel@yahoo.com.br>
2020-07-12 07:10:44 -0300

    Mais otimizações

--- pages/404.js
+++ pages/404.js
@@ -1,12 +1,18 @@
 import Header from '../components/Header'
 import Footer from '../components/Footer'
+import styles from '../components/Error.module.css'
 
 export default function Custom404(){
     return(
         <>
         <Header/>
-        <h1> Pagina de erro 404</h1>
-
+        <section className={styles.content}>
+            
+            <h1>Ops! Não encontramos o que você Procura</h1>
+            <p>Mas continue cuidando de você e das pessoas que você Ama!</p>
+            <h2>Use mascara e alcool gel!</h2>
+            <img srcSet="/assets/img/erro404.jpg" alt="Pagina nao encontrada"/>
+        </section>
         <Footer />
         </>
     )
